51fb4cc423bf3536a0c82276c1d4362815e1f712
Masamichi Takano <masia02@gmail.com>
2018-07-25 01:30:55 +0900

    dappsup

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -25,6 +25,17 @@ class PostInfo extends Component {
         </Link>
 
 
+        <Link to={post.dappsup}>
+          <CardTitle
+          avatar={
+            <Avatar icon={<FontIcon iconClassName="fa fa-bookmark" />} />
+          }
+          title="マイDAppsリストに追加"
+          subtitle={post.dappsup}
+          />
+        </Link>
+
+
         <Link
           className="category-link"
           to={`/categories/${_.kebabCase(post.category)}`}
